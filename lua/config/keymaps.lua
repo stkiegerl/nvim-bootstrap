0200adc58e31a25389775bb997537870a684d39a
stk <git@stk.systems>
2025-05-10 18:41:26 +0200

    configured autoformatting

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -37,13 +37,13 @@ vim.keymap.set('n', '<Right>', ':vertical resize -2<CR>', opts)
 -- Buffers
 vim.keymap.set('n', '<Tab>', ':bnext<CR>', opts)
 vim.keymap.set('n', '<S-Tab>', ':bprevious<CR>', opts)
-vim.keymap.set('n', '<leader>x', ':bdelete!<CR>', opts)   -- close buffer
+vim.keymap.set('n', '<leader>x', ':bdelete!<CR>', opts) -- close buffer
 vim.keymap.set('n', '<leader>b', '<cmd> enew <CR>', opts) -- new buffer
 
 -- Window management
-vim.keymap.set('n', '<leader>wv', '<C-w>v', opts)     -- split window vertically
-vim.keymap.set('n', '<leader>wh', '<C-w>s', opts)     -- split window horizontally
-vim.keymap.set('n', '<leader>we', '<C-w>=', opts)     -- make split windows equal width & height
+vim.keymap.set('n', '<leader>wv', '<C-w>v', opts) -- split window vertically
+vim.keymap.set('n', '<leader>wh', '<C-w>s', opts) -- split window horizontally
+vim.keymap.set('n', '<leader>we', '<C-w>=', opts) -- make split windows equal width & height
 vim.keymap.set('n', '<leader>wx', ':close<CR>', opts) -- close current split window
 
 -- Navigate between splits
@@ -53,10 +53,10 @@ vim.keymap.set('n', '<C-h>', ':wincmd h<CR>', opts)
 vim.keymap.set('n', '<C-l>', ':wincmd l<CR>', opts)
 
 -- Tabs
-vim.keymap.set('n', '<leader>to', ':tabnew<CR>', opts)   -- open new tab
+vim.keymap.set('n', '<leader>to', ':tabnew<CR>', opts) -- open new tab
 vim.keymap.set('n', '<leader>tx', ':tabclose<CR>', opts) -- close current tab
-vim.keymap.set('n', '<leader>tn', ':tabn<CR>', opts)     --  go to next tab
-vim.keymap.set('n', '<leader>tp', ':tabp<CR>', opts)     --  go to previous tab
+vim.keymap.set('n', '<leader>tn', ':tabn<CR>', opts) --  go to next tab
+vim.keymap.set('n', '<leader>tp', ':tabp<CR>', opts) --  go to previous tab
 
 -- Toggle line wrapping
 vim.keymap.set('n', '<leader>lw', '<cmd>set wrap!<CR>', opts)
@@ -73,3 +73,6 @@ vim.keymap.set('v', '<C-j>', ":m '>+1<CR>gv=gv")
 vim.keymap.set('v', '<C-k>', ":m '<-2<CR>gv=gv")
 vim.keymap.set('v', '<C-h>', '<gv')
 vim.keymap.set('v', '<C-l>', '>gv')
+
+-- Formatting
+vim.keymap.set('n', '<leader>cf', function() require('conform').format() end, { desc = 'Format current file' })
--- lua/plugins/autoformat.lua
+++ lua/plugins/autoformat.lua
@@ -1,31 +1,67 @@
 return {
-	"stevearc/conform.nvim",
-	opts = {
-		formatters_by_ft = {
-			lua = { "stylua" },
-			-- Conform will run multiple formatters sequentially
-			python = { "isort", "black" },
-			-- You can customize some of the format options for the filetype (:help conform.format)
-			css = { "prettier" },
-			html = { "prettier" },
-			json = { "prettier" },
-			yaml = { "prettier" },
-			markdown = { "prettier", "doctoc" },
-			shell = { "beautysh", "shfmt" },
-			bash = { "beautysh", "shfmt" },
-			sh = { "beautysh", "shfmt" },
-			sql = { "sql_formatter" },
-			terraform = { "terraform_fmt" },
-			cpp = { "clang-format" },
-			-- Conform will run the first available formatter
-			javascript = { "prettierd", "prettier", stop_after_first = true },
-			typescript = { "prettierd", "prettier", stop_after_first = true },
-		},
-		format_on_save = {
-			-- These options will be passed to conform.format()
-			timeout_ms = 500,
-			lsp_format = "fallback",
-		},
-	},
-}
+    {
+        'mason-org/mason.nvim',
+        config = function() require('mason').setup() end,
+    },
 
+    -- Install all formatters
+    {
+        'WhoIsSethDaniel/mason-tool-installer.nvim',
+    },
+
+    {
+        -- Configure formatters
+        'stevearc/conform.nvim',
+        dependencies = {
+            'mason-org/mason.nvim',
+            'WhoIsSethDaniel/mason-tool-installer',
+        },
+        config = function()
+            require('mason-tool-installer').setup {
+                ensure_installed = {
+                    'stylua',
+                    'isort',
+                    'black',
+                    'prettier',
+                    'doctoc',
+                    'beautysh',
+                    'shfmt',
+                    'sql-formatter',
+                    'terraform',
+                    'rustfmt',
+                    'clang-format',
+                    'prettierd',
+                },
+                run_on_start = false,
+            }
+            require('conform').setup {
+                formatters_by_ft = {
+                    lua = { 'stylua' },
+                    -- Conform will run multiple formatters sequentially
+                    python = { 'isort', 'black' },
+                    -- You can customize some of the format options for the filetype (:help conform.format)
+                    css = { 'prettier' },
+                    html = { 'prettier' },
+                    json = { 'prettier' },
+                    yaml = { 'prettier' },
+                    markdown = { 'prettier', 'doctoc' },
+                    shell = { 'beautysh', 'shfmt' },
+                    bash = { 'beautysh', 'shfmt' },
+                    sh = { 'beautysh', 'shfmt' },
+                    sql = { 'sql_formatter' },
+                    terraform = { 'terraform_fmt' },
+                    rust = { 'rustfmt' },
+                    cpp = { 'clang-format' },
+                    -- Conform will run the first available formatter
+                    javascript = { 'prettierd', 'prettier', stop_after_first = true },
+                    typescript = { 'prettierd', 'prettier', stop_after_first = true },
+                },
+                format_on_save = {
+                    -- These options will be passed to conform.format()
+                    timeout_ms = 1000,
+                    lsp_format = 'fallback',
+                },
+            }
+        end,
+    },
+}
