3b2b008b4f6c9ddb9ab99826898f5c4631bba36a
stk <git@stk.systems>
2025-05-10 02:29:03 +0200

    updated autocompletion

--- lua/config/lsp-config.lua
+++ lua/config/lsp-config.lua
@@ -61,6 +61,9 @@ vim.api.nvim_create_autocmd('LspAttach', {
     callback = function(ev)
         local client = vim.lsp.get_client_by_id(ev.data.client_id)
         if client:supports_method 'textDocument/completion' then
+            -- Optional: trigger autocompletion on EVERY keypress. May be slow!
+            local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
+            client.server_capabilities.completionProvider.triggerCharacters = chars
             vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
         end
     end,
