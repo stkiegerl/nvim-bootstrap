b7fa475f385fbf6ec9014dde1aec15ceeb79ac79
stk <git@stk.systems>
2025-05-11 04:42:32 +0200

    added additional functioniality from kickstart.nvim

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -4,6 +4,9 @@ return {
         dependencies = {
             { 'mason-org/mason.nvim', opts = {} },
             'mason-org/mason-lspconfig.nvim',
+            -- Useful status updates for LSP.
+            -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
+            { 'j-hui/fidget.nvim', opts = {} },
         },
         config = function()
             local servers = {
@@ -51,32 +54,119 @@ return {
                 },
             }
 
-            -- Auto-install and enable LSPs with Mason
+            -- Auto-install LSPs with Mason
             local ensure_installed = vim.tbl_keys(servers)
             require('mason-lspconfig').setup {
                 ensure_installed = ensure_installed,
+                automatic_enable = false,
             }
 
-            -- Apply clustom configs to LSPs
+            -- Apply clustom configs and enable LSPs
             for name, cfg in pairs(servers) do
+                vim.lsp.enable(name)
                 if next(cfg) then vim.lsp.config(name, cfg) end
             end
 
-            -- Uncomment to enable Neovims native autocompletion(Optional) -> Using blink for autocompletion (see plugins/autocompletion)
-            -- vim.api.nvim_create_autocmd('LspAttach', {
-            --     callback = function(ev)
-            --         local client = vim.lsp.get_client_by_id(ev.data.client_id)
-            --         if client:supports_method 'textDocument/completion' then
-            --             -- Optional: trigger autocompletion on EVERY keypress. May be slow!
-            --             local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
-            --             client.server_capabilities.completionProvider.triggerCharacters = chars
-            --             vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
-            --         end
-            --     end,
-            -- })
+            --  This function gets run when an LSP attaches to a particular buffer.
+            --    That is to say, every time a new file is opened that is associated with
+            --    an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
+            --    function will be executed to configure the current buffer
+            vim.api.nvim_create_autocmd('LspAttach', {
+                group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
+                callback = function(event)
+                    -- NOTE: Remember that Lua is a real programming language, and as such it is possible
+                    -- to define small helper and utility functions so you don't have to repeat yourself.
+                    --
+                    -- In this case, we create a function that lets us more easily define mappings specific
+                    -- for LSP related items. It sets the mode, buffer and description for us each time.
+                    local map = function(keys, func, desc, mode)
+                        mode = mode or 'n'
+                        vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+                    end
+
+                    -- This function resolves a difference between neovim nightly (version 0.11) and stable (version 0.10)
+                    ---@param client vim.lsp.Client
+                    ---@param method vim.lsp.protocol.Method
+                    ---@param bufnr? integer some lsp support methods only in specific files
+                    ---@return boolean
+                    local function client_supports_method(client, method, bufnr)
+                        if vim.fn.has 'nvim-0.11' == 1 then
+                            return client:supports_method(method, bufnr)
+                        else
+                            return client.supports_method(method, { bufnr = bufnr })
+                        end
+                    end
+
+                    -- The following two autocommands are used to highlight references of the
+                    -- word under your cursor when your cursor rests there for a little while.
+                    --    See `:help CursorHold` for information about when this is executed
+                    --
+                    -- When you move your cursor, the highlights will be cleared (the second autocommand).
+                    local client = vim.lsp.get_client_by_id(event.data.client_id)
+                    if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
+                        local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
+                        vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+                            buffer = event.buf,
+                            group = highlight_augroup,
+                            callback = vim.lsp.buf.document_highlight,
+                        })
+
+                        vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+                            buffer = event.buf,
+                            group = highlight_augroup,
+                            callback = vim.lsp.buf.clear_references,
+                        })
+
+                        vim.api.nvim_create_autocmd('LspDetach', {
+                            group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
+                            callback = function(event2)
+                                vim.lsp.buf.clear_references()
+                                vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
+                            end,
+                        })
+                    end
+
+                    -- The following code creates a keymap to toggle inlay hints in your
+                    -- code, if the language server you are using supports them
+                    --
+                    -- This may be unwanted, since they displace some of your code
+                    if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf) then
+                        map(
+                            '<leader>th',
+                            function() vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf }) end,
+                            '[T]oggle Inlay [H]ints'
+                        )
+                    end
+                end,
+            })
 
             -- Show diagnostic messages (errors & warnings) inline
-            vim.diagnostic.config { virtual_text = true }
+            vim.diagnostic.config {
+                severity_sort = true,
+                float = { border = 'rounded', source = 'if_many' },
+                underline = { severity = vim.diagnostic.severity.ERROR },
+                signs = vim.g.have_nerd_font and {
+                    text = {
+                        [vim.diagnostic.severity.ERROR] = '󰅚 ',
+                        [vim.diagnostic.severity.WARN] = '󰀪 ',
+                        [vim.diagnostic.severity.INFO] = '󰋽 ',
+                        [vim.diagnostic.severity.HINT] = '󰌶 ',
+                    },
+                } or {},
+                virtual_text = {
+                    source = 'if_many',
+                    spacing = 2,
+                    format = function(diagnostic)
+                        local diagnostic_message = {
+                            [vim.diagnostic.severity.ERROR] = diagnostic.message,
+                            [vim.diagnostic.severity.WARN] = diagnostic.message,
+                            [vim.diagnostic.severity.INFO] = diagnostic.message,
+                            [vim.diagnostic.severity.HINT] = diagnostic.message,
+                        }
+                        return diagnostic_message[diagnostic.severity]
+                    end,
+                },
+            }
         end,
     },
 }
